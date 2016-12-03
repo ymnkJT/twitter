6083d4d0ac9ccf2e15714f754339290ddb366c9d
Satomi Yamanaka <nui.mea5en@gmail.com>
2016-12-03 07:25:34 +0000

    Inplement create tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,18 @@
 class TweetsController < ApplicationController
   def index
+    @tweet = Tweet.new
   end
+  
+  def new
+  end
+  
+  def create
+    Tweet.create(tweets_params)
+    redirect_to tweets_path
+  end
+  
+  private
+    def tweets_params
+      params.require(:tweet).permit(:content)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get 'tweets' => 'tweets#index'
+  post 'tweets' => 'tweets#create'
 end
