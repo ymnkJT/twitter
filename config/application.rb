d3dbff2a2eb6973a2347590c4d99ada1dea06880
Satomi Yamanaka <nui.mea5en@gmail.com>
2016-12-06 09:49:45 +0000

    Add bootstrap to header

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module Twitter
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.action_view.field_error_proc = proc { |html_tag, instance| html_tag }
+    config.assets.paths << "#{Rails}/vendor/assets/fonts"
   end
 end
