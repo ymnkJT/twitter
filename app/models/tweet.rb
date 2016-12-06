4d4938dde9a5a97ff7feb683ed5f17f910361989
Satomi Yamanaka <nui.mea5en@gmail.com>
2016-12-06 13:57:53 +0000

    Fix validation messeges

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,8 @@
 class Tweet < ActiveRecord::Base
   validates :content, length: {
     minimum: 1,
-    maximum: 10,
+    maximum: 255,
+    too_short: "空白では投稿できません。1文字以上入力して下さい。",
+    too_long: "投稿は255バイト（全角127文字）以内にして下さい"
   }
 end
